0cab1210a28c9fb469f15c28330eab617d2417d9
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2024-06-17 13:43:31 +0300

    Changes to be committed:     new file:   Courses/JS practice HTTP/HTTP/request.py 	deleted:    Courses/JS practice HTTP/HTTP/weather_api.js 	new file:   TestProject/GUI.py 	new file:   TestProject/image_generation.py

---
+++ Courses/JS practice HTTP/HTTP/request.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def main():
+    content = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?id=625143&appid=8db44f8b758f996a9139941aa44f6877')
+    data_dict = content.json()
+
+    for key in data_dict.keys():
+        print(key, '=', data_dict.get(key))
+
+
+main()
---
+++ TestProject/GUI.py
@@ -0,0 +1,8 @@
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.gridlayout import MDGridLayout
+
+
+class MainScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
