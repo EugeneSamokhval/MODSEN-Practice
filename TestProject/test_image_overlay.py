39b17ee4b7c7ed6ae7c9573ddd571a82b669e592
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2024-07-02 09:20:31 +0300

    modified:   TestProject/test_image_overlay.py

--- TestProject/test_image_overlay.py
+++ TestProject/test_image_overlay.py
@@ -18,8 +18,7 @@ class TestImageOverlay(unittest.TestCase):
         Check input type.
         """
         image = np.random.randint(0, 256, (100, 100, 3), dtype=np.uint8)
-        result = images_overlay.overlay_text(
-            image, 1, 10, 'Hello world', font_path='./fonts/new-standard-font-collection/ageo-32909/AgeoTrialBold-3zPA3.ttf')
+        result = images_overlay.overlay_text(image, 1, 10, 'Hello world', font_size=10, font_path='./fonts/new-standard-font-collection/ageo-32909/AgeoTrialBold-3zPA3.ttf')
         self.assertEqual(type(result), np.array)
 
     def check_load_fonts(self):
